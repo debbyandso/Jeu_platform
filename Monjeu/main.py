074dece5042416a08717ab43166b7875bc862ea6
debbyandso <46191132+debbyandso@users.noreply.github.com>
2020-10-06 18:11:12 +0200

    added character

--- Monjeu/main.py
+++ Monjeu/main.py
@@ -1,12 +1,27 @@
 import pygame
 pygame.init()
 
+#classe representant le joueur
+
+class Player (pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 10
+        #vitesse deplacement du joeur
+        self.velocity= 5
+        self.image = pygame.image.load('assetsjeuplatform/Character.png')
+        self.rect= self.image.get_rect()
+
 
 # 1/ GENERER LES FENETRES 
 pygame.display.set_caption('Les aventures de Nono')
-screen = pygame.display.set_mode((1080,1000))
+screen = pygame.display.set_mode((1920,1080))
 # 2/ Importation d'un background pour le jeu
 background= pygame.image.load('assetsjeuplatform/Background.png')
+#charger player ( variable player =  nouvelle instance player)
+player = Player()
 running = True
 
 # 3/ boucles pour la fenetre reste ouverte running
@@ -16,6 +31,10 @@ while running:
     #(0.0), c'est pour que le background soit centré et ensuite, ca fait des zooms selo les valeurs
     screen.blit(background, (0,0))
 
+    #appliquer image du joueur ( player = classe)
+
+    screen.blit(player.image, player.rect)
+
     #mettre a jour ecran
     pygame.display.flip()
 
@@ -24,3 +43,5 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+
+#video 8m55
